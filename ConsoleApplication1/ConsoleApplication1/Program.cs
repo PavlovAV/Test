7da51d2eeacf251674f34d8f476fdb35493b7dae
Alexandr Pavlov <a.palov@sgbi.ru>
2017-09-06 16:44:45 +0300

    commit master2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("testBranch2");
+            Console.WriteLine("testMaster2");
         }
     }
 }
